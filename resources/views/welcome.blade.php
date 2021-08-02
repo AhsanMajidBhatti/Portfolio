1613bb48e319a464a523f0c34259ceb52bd1a8f4
Ahsan Majid Bhatti <ahsanmajidbhatti49@gmail.com>
2021-08-02 19:35:38 +0500

    site links added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -50,7 +50,6 @@
         <li><a href="#skills" class="nav-link scrollto"><i class="bx bx-wrench"></i> <span>Skill</span></a></li>
         <li><a href="#resume" class="nav-link scrollto"><i class="bx bx-file-blank"></i> <span>Resume</span></a></li>
         <li><a href="#services" class="nav-link scrollto"><i class="bx bx-server"></i> <span>Services</span></a></li>
-        <li><a href="#contact" class="nav-link scrollto"><i class="bx bx-envelope"></i> <span>Contact</span></a></li>
       </ul>
     </nav><!-- .nav-menu -->
 
@@ -62,11 +61,8 @@
       <h1>{{ $profile != null ? $profile->name : '' }}</h1>
       <p>I'm <span class="typed" data-typed-items="Full-Stack Web Developer, Programmer"></span></p>
       <div class="social-links">
-        <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-        <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-        <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-        <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-        <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+        <a href="https://twitter.com/ahsanmajidbhat2" class="twitter"><i class="bx bxl-twitter"></i></a>
+        <a href="https://www.facebook.com/ahsanmajid.bhatti" class="facebook"><i class="bx bxl-facebook"></i></a>
       </div>
     </div>
   </section><!-- End Hero -->
@@ -233,7 +229,7 @@
     </section><!-- End Services Section -->
 
     <!-- ======= Contact Section ======= -->
-    <section id="contact" class="contact">
+    <!-- <section id="contact" class="contact">
       <div class="container" data-aos="fade-up">
 
         <div class="section-title">
@@ -296,31 +292,21 @@
         </div>
 
       </div>
-    </section><!-- End Contact Section -->
+    </section> -->
+    <!-- End Contact Section -->
 
   </main><!-- End #main -->
 
   <!-- ======= Footer ======= -->
   <footer id="footer">
     <div class="container">
-      <h3>Brandon Johnson</h3>
-      <p>Et aut eum quis fuga eos sunt ipsa nihil. Labore corporis magni eligendi fuga maxime saepe commodi placeat.</p>
+      <h3>{{ $profile != null ? $profile->name : '' }}</h3>
       <div class="social-links">
-        <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-        <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-        <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-        <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-        <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-      </div>
-      <div class="copyright">
-        &copy; Copyright <strong><span>MyResume</span></strong>. All Rights Reserved
+        <a href="https://twitter.com/ahsanmajidbhat2" class="twitter"><i class="bx bxl-twitter"></i></a>
+        <a href="https://www.facebook.com/ahsanmajid.bhatti" class="facebook"><i class="bx bxl-facebook"></i></a>
       </div>
       <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you purchased the pro version. -->
-        <!-- Licensing information: [license-url] -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/free-html-bootstrap-template-my-resume/ -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+        Designed by Ahsan Majid Bhatti
       </div>
     </div>
   </footer><!-- End Footer -->
